d3352546accd68ebec36acd6c43066c0e0061e94
Carlos de Paula <me@carlosedp.com>
2025-06-16 18:15:45 -0300

    Add minimap to the waveform

--- src/Waveform.tsx
+++ src/Waveform.tsx
@@ -291,6 +291,13 @@ const Waveform = forwardRef<WaveformRef, WaveformProps>(
               return `${minutes}:${secs < 10 ? "0" : ""}${secs}.${millis}`;
             },
           }),
+          Minimap.create({
+            height: 20,
+            waveColor: theme.palette.primary.main,
+            progressColor: theme.palette.primary.light,
+            overlayColor: theme.palette.primary.dark,
+            dragToSeek: true,
+          }),
         ],
       });
 
