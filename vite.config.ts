2c573d37ce9d773a9f5317ffe24cf5e0e798edce
Carlos de Paula <me@carlosedp.com>
2025-06-27 18:25:49 -0300

    Dynamically bundle rubberband.wasm module

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './', // Ensure relative paths for Electron
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/rubberband-wasm/dist/rubberband.wasm',
+          dest: '',
+        },
+      ],
+    }),
+  ],
   assetsInclude: ['**/*.wasm'],
 });
